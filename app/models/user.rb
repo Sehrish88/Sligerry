51b4170ae580a6bc485b65df11769a12f2b68a13
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-06-03 16:53:44 -0400

    create users, schedules, and courses table, also add models

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+     
+   
+    has_many :schedules
+    has_many :courses, through: :schedules 
+    
+    has_secure_password
+    validates_confirmation_of :password
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :email, uniqueness: true 
+
+end
