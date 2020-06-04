85ce6c6b9f53c066e2ddc4c4955e7dbc9683e097
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-06-04 11:25:41 -0400

    add actions to instructors controller

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -1,2 +1,26 @@
 class InstructorsController < ApplicationController
+    def show
+        @instructor = Instructor.find(params[:id]) 
+    end 
+
+    def new
+      @instructor = Instructor.new 
+
+    end 
+
+    def create
+        @instructor = Instructor.new(instructor_params)
+        if @instructor.save
+          session[:instructor_id] = @instrutor.id
+          redirect_to instructor_path(@instructor)
+        else
+          render :new
+        end
+      end
+    
+      private
+    
+      def student_params
+        params.require(:instructor).permit(:name, :email, :password, :password_confirmation)
+      end
 end
