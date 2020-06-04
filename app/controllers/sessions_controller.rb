27f041cc100c06ec391b6ea12b7812103a59c243
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-06-04 18:26:34 -0400

    fix typo in create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
 
     def create
-        @instructor = Igit add nstructor.find_by(email: params[:instructor][:email])
+        @instructor = Instructor.find_by(email: params[:instructor][:email])
         if @instructor && @instructor.authenticate(params[:instructor][:password])
             session[:instructor_id] = @instructor.id
             redirect_to instructor_path(@instructor)
