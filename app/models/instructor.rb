1cb7a2b0d112f2418e04ac101b00739996c31f84
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-06-04 14:20:55 -0400

    delete line for accepting nested attributes for courses

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -2,9 +2,10 @@ class Instructor < ApplicationRecord
     has_many :courses
     has_many :schedules, through: :courses  
     
-    accepts_nested_attributes_for :courses 
+    #accepts_nested_attributes_for :courses 
     
     has_secure_password
+
     validates_confirmation_of :password
     validates :name, presence: true
     validates :email, presence: true
