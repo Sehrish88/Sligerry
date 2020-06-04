00b8d35743393b63545d2531f8d6d3eb0a745c49
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-06-04 18:02:53 -0400

    fix typos in config.ru routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,17 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home' 
   get 'about', to: 'pages#about'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+
   resources :instructors
   resources :schedules 
   resources :courses 
   resources :instructors do
     resources :courses    
   end 
-
-  post '/logout', to: 'sessions#destroy'
-
+  
+  
+ 
 end
