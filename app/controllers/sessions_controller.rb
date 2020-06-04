64ed169e333f098a0ecc5634af305bf3b1bcb3cf
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-06-04 18:09:55 -0400

    add create and destroy actions to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,21 @@
 class SessionsController < ApplicationController
 
     def create
+        @instructor = instructor.find_by(email: params[:instructor][:email])
+        if @instructor && @instructor.authenticate(params[:instructor][:password])
+            session[:instructor_id] = @instructor.id
+            redirect_to instructor_path(@instructor)
+        else
+            render :new
+        end
+      end 
+
+    def new 
 
     end 
 
     def destroy
-
-    end 
+        session.delete :user_id
+        redirect_to '/'
+    end
 end
