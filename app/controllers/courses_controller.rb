d17a44fd0c3d1f6d4934bc24dd2028087ff4324e
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-06-04 18:06:45 -0400

    add new, show, and create actions to courses controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,29 @@
 class CoursesController < ApplicationController
+     def index 
+
+     end 
+
+     def show
+        @course = Course.find_by(id: params[:id])
+     end 
+
+     def new 
+        @course = Course.new 
+     end 
+
+     def create 
+        @course = Course.new(course_params)
+        if @course.save
+            redirect_to course_path(@course)
+        else 
+            render :new
+        end 
+     end 
+
+     private
+
+     def course_params
+        params.require(:course).permit(:title, :description)
+     end 
+
 end
