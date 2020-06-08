2fba24be0e67639cf9be5c99a6446a6e848221ec
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-06-08 14:19:44 -0400

    add helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :current_instructor 
+    #
+    def current_instructor
+      Instructor.find_by(id: session[:instructor_id])
+    end
+  
+    def require_login
+      if current_instructor
+        current_instructor 
+      else
+          redirect_to '/'
+        end
+    end
+
 end
