24120411aad4faa48c447330415e769d1b541bd6
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-06-08 14:21:09 -0400

    add code to find instructor in view pages

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,29 +1,57 @@
 class CoursesController < ApplicationController
+
+  
+
+
      def index 
+      if !params[:instructor_id]
+         @courses = Course.all 
+      else 
+         #check if instructor exists 
+         instructor = Instructor.find_by(id: params[:id]) 
+         @courses = instructor.courses
+      end 
+    end 
 
+     def show 
+         @instructor = Instructor.find_by(id: params[:instructor_id]) 
+         @course = Course.find_by(id: params[:id]) 
+      
      end 
 
-     def show
-        @course = Course.find_by(id: params[:id])
-     end 
-
-     def new 
+     def new  
+      #byebug
+      
         @course = Course.new 
+        @course.build_schedule 
+        
      end 
 
-     def create 
-        @course = Course.new(course_params)
+     def create
+       #byebug
+        
+        @course = Course.create(course_params)
+        @course.instructor = current_instructor 
         if @course.save
-            redirect_to course_path(@course)
+            redirect_to instructor_course_path(@course.instructor, @course) 
         else 
             render :new
         end 
      end 
 
+     def edit 
+
+     end 
+
+     def update 
+
+     end 
+     
+
      private
 
      def course_params
-        params.require(:course).permit(:title, :description)
+        params.require(:course).permit(:title, :description, schedule_attributes: [:days, :time, :semester])
      end 
 
 end
