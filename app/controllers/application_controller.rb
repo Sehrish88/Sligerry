f48926dfbe13b6f8acab64e85af1b371672375ec
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-06-13 14:06:27 -0400

    add helper methods to DRY up code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,20 @@
 class ApplicationController < ActionController::Base
 
-    helper_method :current_instructor 
+    helper_method :current_instructor
+    helper_method :find_course 
+    helper_method :find_instructor 
     #
     def current_instructor
       Instructor.find_by(id: session[:instructor_id])
     end
+
+    def find_course 
+      @course = Course.find_by(id: params[:id])
+    end 
+
+    def find_instructor 
+      @instructor = Instructor.find(params[:id]) 
+    end 
   
     def require_login
       if current_instructor
@@ -13,5 +23,7 @@ class ApplicationController < ActionController::Base
           redirect_to '/'
         end
     end
+
+    
 
 end
