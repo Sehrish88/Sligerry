f267db17e1d5d31760925c7a50f20d24bae1afb8
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-06-13 14:13:21 -0400

    add validations and scope method to course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,5 +2,13 @@ class Course < ApplicationRecord
     belongs_to :instructor
     belongs_to :schedule 
 
+    validates :title, presence: true
+    validates :title, uniqueness: true 
+    validates :description, presence: true 
+    
+
     accepts_nested_attributes_for :schedule 
+
+    scope :course_type, -> (query) { where("description LIKE ?", "%" + query + "%") } 
+    
 end 
