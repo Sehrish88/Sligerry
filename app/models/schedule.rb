5fc4f2cc62d6b409a4c4b1e65a751c6c89a75278
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-06-13 14:16:37 -0400

    add validations

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,4 +1,8 @@
 class Schedule < ApplicationRecord
     has_many :courses
     has_many :instructors, through: :courses
+
+    validates :days, presence: true 
+    validates :time, presence: true
+    validates :semester, presence: true 
 end
